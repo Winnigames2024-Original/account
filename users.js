66cec8c597368262ed8d8946cf04dff71e9c2426
link-short <dora.family228@gmail.com>
2025-12-10 16:14:23 +0500

    Create users.js

---
+++ users.js
@@ -0,0 +1,54 @@
+const urlPaframs = new URLSearchParams(window.location.search);
+const user = urlPaframs.get('account');
+const UserName = document.getElementById("h2Username");
+
+// Winnigames2024 Account
+const validUsername = 'Winnigames2024';
+const lWinnigames2024VisibleUsername = "Winnigames2024-Original %(*OWNER*)%";
+// Winnigames2024 Test Account
+const validTestUsername = 'Winnigames2024Test';
+const TestAccountVisibleUsername = 'Unauthorized Users';
+// Liliya Account
+const validLiliyaUsername = 'LiliyaIO';
+const LiliyaIOVisibleUsername = 'Liliya Lonskaya %(*MODERATOR*)%';
+
+if (user) {
+  // Winnigames2024 Account Checker
+  if (user == validUsername) {
+    UserName.textContent = Winnigames2024VisibleUsername;
+  }
+  // Winnigames2024 Test Account Checker
+  if (user == validTestUsername) {
+    UserName.textContent = TestAccountVisibleUsername;
+  }
+  // LiliyaIO Account Checker
+  if (user == validLiliyaUsername) {
+    UserName.textContent = LiliyaIOVisibleUsername;
+  }
+}
+
+
+
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(';'); // Split the document.cookie string into an array of individual cookie strings
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') { // Remove leading whitespace from the cookie string
+      c = c.substring(1);
+    }
+    if (c.indexOf(nameEQ) === 0) { // Check if the cookie string starts with the desired name
+      return c.substring(nameEQ.length, c.length); // Extract and return the cookie's value
+    }
+  }
+  return ""; // Return an empty string if the cookie is not found
+}
+
+
+function setCookie(name, value, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000)); // Calculate expiration date
+  const expires = "expires=" + d.toUTCString();
+  document.cookie = name + value + ";" + expires + ";path=./account";
+}

acc3d304756e567033ef26650e2269166c69bb89
link-short <dora.family228@gmail.com>
2025-12-10 15:54:23 +0500

    Update script.js

--- dashboard/script.js
+++ dashboard/script.js
@@ -2,6 +2,8 @@ const urlParams = new URLSearchParams(window.location.search);
 const user = urlParams.get('account');
 const UserName = document.getElementById("h2Username");
 
+InitalizeDashboard();
+
 // Winnigames2024 Account
 const validUsername = 'Winnigames2024';
 const validPassword = 'kodiki8909';
@@ -44,25 +46,6 @@ if (user) {
     }
   }
 }
-
-if (type == "cookieDialog") {
-  const fileName = urlParams.get('file');
-  const visibleAlert = urlParams.get('alert');
-  if (fileName == "winnicookies.cookie") {
-    if (visibleAlert == "true") {
-      alert("Cookie message visible now");
-    }
-    else {
-      console.log("Cookie message visible now");
-      console.log(type);
-      console.log(fileName);
-    }
-    console.log("Cookie Visible Now!!!");
-    console.log(type);
-    console.log(fileName);
-  }
-}
-
 
 
 // Functions
